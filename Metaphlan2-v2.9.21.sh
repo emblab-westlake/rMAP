d67845d360f5d6a734c3d3a9ca2641a1c3361d2d
emblab <emblab@westlake.edu.cn>
2019-09-19 16:06:16 +0800

    For Metaphlan2 V2.9.21

---
+++ Metaphlan2-v2.9.21.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+##########################################################################
+# Author: zhangguoqing
+# Email: emblab@westlake.edu.cn
+# Lab: EMBLab Westlake University
+# Date:2019-9-7
+# Version: 1.0
+# Update:2019-9-10
+#
+# Function: Use Metaphlan2 to Annotating to species
+
+# Copyright (C) 2019  EMBLab Westlake University
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+##########################################################################
+
+#set -o nounset
+help()
+{
+    cat <<- EOF
+    Desc: The shell script USES Metaphlan2 to Annotating to species
+          Relative paths are used internally within the script, please run this script under your PROJECT PATH.
+
+    Usage of (default TASK num): ./Step5-Metaphlan2.sh list.txt
+    Usage of (Custom TASK num Parameters): ./Step5-Metaphlan2.sh -list list.txt -core 10
+    
+    list.txt is Samplenames list file; {--core} specifies the number of concurrent tasks. Task num is 60(default), total thread is 60
+    Email: emblab@westlake.edu.cn
+    Lab: EMBLab westlake university
+    License: GPL
+EOF
+    exit 0
+
+}
+
+samplelist=$1
+Nproc=60 #task num
+
+
+while [ -n "$1" ]; do
+    case $1 in
+        -h) help;; # function help is called
+        --help) help;; # function help is called
+        --) shift;break;; # end of options
+        -list) samplelist=$2;break;; #list.txt file
+esac
+done
+
+while [ -n "$3" ]; do
+    case $3 in
+        -core) Nproc=$4;break;; #list.txt file
+esac
+done
+
+
+
+Alignedpath="AlignedData" 
+
+obojectpath="Metaphlan2Result"
+mkdir ${obojectpath}
+
+
+echo "Metaphlan2 start ----------- `date`" > Metaphlan2-log.file
+
+source $HOME/miniconda3/bin/activate metaphlan2
+for samplename in `cat list.txt`;do
+rm ${Alignedpath}/${samplename}/${samplename}_metagenome.bowtie2.bz2
+metaphlan2.py ${Alignedpath}/${samplename}/${samplename}_filtered1.fastq,${Alignedpath}/${samplename}/${samplename}_filtered2.fastq \
+--bowtie2out ${Alignedpath}/${samplename}/${samplename}_metagenome.bowtie2.bz2 --nproc ${Nproc} \
+--input_type fastq > ${obojectpath}/${samplename}_profiled_metagenome.txt
+echo "${samplename} has been classed ------------------------ `date`" >> Metaphlan2-log.file
+done
+echo "All samples have been classed ------------------------ `date`" >> Metaphlan2-log.file
+source $HOME/miniconda3/bin/deactivate
